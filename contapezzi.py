41c3f407f23dd6c776264f89ee4095af6764a8fd
gian74Git <giancarlo.caravenghi@gmail.com>
2016-11-26 13:10:35 +0100

    New internal_init function to manage day change

--- contapezzi.py
+++ contapezzi.py
@@ -1,10 +1,12 @@
 from contaPezziLogic import logicCounter
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow
 from PyQt5 import QtWidgets
 from contapezzi_ui import Ui_contaPezzi
 import time
 from PyQt5 import QtGui, QtCore
+from const import CUSTOMER_NAME
+from datetime import datetime
 
 
 Debug = True
@@ -14,7 +16,8 @@ if not Debug:
 
 class PieceCounterGui(QMainWindow):
     def __init__(self):
-
+        self.dateNow = datetime.now().strftime("%Y-%m-%d")
+        self.datePassed = datetime.now().strftime("%Y-%m-%d")
         self.red_gradient = "background-color: qlineargradient(spread:pad, x1:0.505, y1:1, x2:0.505, y2:0, stop:0 " \
                             "rgba(174, 0, 5, 255), stop:1 rgba(255, 2, 0, 255));"
         self.green_gradient = "background-color: qlineargradient(spread:pad, x1:0.505, y1:1, x2:0.505, y2:0, stop:0 " \
@@ -26,31 +29,33 @@ class PieceCounterGui(QMainWindow):
         self.ui = Ui_contaPezzi()
         self.ui.setupUi(self)
 
-        self.daily_qty = self.logic.get_daily_qty()
-        self.ui.lbPzGiorn.setText(str(self.daily_qty))
-        self.ui.leNumPezzi.setText(str(self.daily_qty))
-        self.logic.set_daily_qty()
-        #self.draw_hours_and_qty()
-
         self.ui.btnChiudi.clicked.connect(self.btnEsciClick)
         self.ui.actionEsci.triggered.connect(self.btnEsciClick)
-        self.ui.lbCliente.setText("VIST TECH")
-        self.ui.lbData.setText(time.strftime("%d/%m/%Y"))
+        self.ui.lbCliente.setText(CUSTOMER_NAME)
 
         self.ui.btnTest.clicked.connect(self.btn_test_click)
 
         self.ui.btnImposta.clicked.connect(self.btn_sethours_click)
-        self.ui.lbPzFatti.setText(str(self.logic.get_pieces_until_now()))
-        self.draw_hours_and_qty()
-        #Timer declaration to control changing day. Fired every minute
+
+        # Timer declaration to control changing day. Fired every minute
         self.timer_day_change = QtCore.QTimer(self)
         self.timer_day_change.timeout.connect(self.update_for_day_change)
-        self.timer_day_change.start(60000)
+        self.timer_day_change.start(1000)
 
-        #This timer provide update of preview pieces till now.
+        # This timer provide update of preview pieces till now. Fired every secon but we can slow it.
         self.update_preview_tot = QtCore.QTimer(self)
         self.update_preview_tot.timeout.connect(self.update_preview_now)
         self.update_preview_tot.start(1000)
+        self.internal_init()
+
+    def internal_init(self):
+        self.daily_qty = self.logic.get_daily_qty()
+        self.ui.lbPzGiorn.setText(str(self.daily_qty))
+        self.ui.leNumPezzi.setText(str(self.daily_qty))
+        self.logic.set_daily_qty()
+        self.ui.lbData.setText(time.strftime("%d/%m/%Y"))
+        self.ui.lbPzFatti.setText(str(self.logic.get_pieces_until_now()))
+        self.draw_hours_and_qty()
         self.update_preview_now(True)
 
     def update_preview_now(self, do_anyway = False):
@@ -64,30 +69,41 @@ class PieceCounterGui(QMainWindow):
     def control_color_pcs_done(self, dict_passed):
         preview_pcs_hour = self.logic.get_preview_pieces_this_hour(dict_passed["HOUR_START"], dict_passed["HOUR_END"])
         done_pcs_hour = int(dict_passed["qty_hour"]);
-        if (done_pcs_hour < preview_pcs_hour):
+        if done_pcs_hour < preview_pcs_hour:
             dict_passed["label_pcs_done"].setStyleSheet(self.red_gradient)
         else:
             dict_passed["label_pcs_done"].setStyleSheet(self.green_gradient)
 
+    # Clearing QFrames
+    def clear_qframe(self, qframe):
+        for i in reversed(range(qframe.count())):
+            # 0 is the title label
+            if i > 0:
+                qframe.itemAt(i).widget().deleteLater()
+
     def draw_hours_and_qty(self):
+        self.clear_qframe(self.ui.vlOre)
+        self.clear_qframe(self.ui.vlPrev)
+        self.clear_qframe(self.ui.vlFatti)
+
         self.lista_orari = self.logic.get_hours()
         font = QtGui.QFont()
         font.setPointSize(24)
         for dict_orario in self.lista_orari:
+            # N.B.: vl_ore it's the name of the layout of the object qfOre in QT Designer
             self.ui.lbTitoloOra = QtWidgets.QLabel(self.ui.qfOre)
             self.ui.lbTitoloOra.setText(dict_orario["ORARIO"])
             self.ui.lbTitoloOra.setFont(font)
             self.ui.vlOre.addWidget(self.ui.lbTitoloOra)
 
-            #Label generation for preview pieces
-            #N.B.: vl_ore it's the name of the layout of the object qfOre in QT Designer
-            self.ui.lbQtyPerOra = QtWidgets.QLabel(self.ui.qfOre)
+            # Label generation for preview pieces
+            self.ui.lbQtyPerOra = QtWidgets.QLabel(self.ui.qfPrev)
             self.ui.lbQtyPerOra.setText(str(int(dict_orario["qty_expected"])))
             self.ui.lbQtyPerOra.setFont(font)
             self.ui.vlPrev.addWidget(self.ui.lbQtyPerOra)
             self.ui.lbQtyPerOra.setAlignment(QtCore.Qt.AlignCenter)
 
-            #Label generation for done pieces.
+            # Label generation for done pieces.
             self.ui.vlFatti.addWidget(dict_orario["label_pcs_done"])
             dict_orario["label_pcs_done"].setFont(font)
             dict_orario["label_pcs_done"].setText(str(int(dict_orario["qty_hour"])))
@@ -119,17 +135,18 @@ class PieceCounterGui(QMainWindow):
         self.close()
 
     def closeEvent(self, event):
-        s_msg = 'Vuoi veramente uscire dal programma ?'
-        reply = QMessageBox.question(self, 'Messaggio:', s_msg, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        s_msg = "Vuoi veramente uscire dal programma ?"
+        reply = QMessageBox.question(self, "Messaggio:", s_msg, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if reply == QMessageBox.Yes:
             event.accept()
         else:
             event.ignore()
 
     def update_for_day_change(self):
-        #Vedi def update_clock(self, firstLaunch=False): di contaPezzi. Svuotare anche le liste e ricrearle...
-        if (self.dateNow != self.datePassed):
-            self.ui.lbPzFatti.setText("0")
+        self.dateNow = datetime.now().strftime("%Y-%m-%d")
+        if self.dateNow != self.datePassed:
+            self.datePassed = datetime.now().strftime("%Y-%m-%d")
+            self.internal_init()
 
 app = QApplication(sys.argv)
 ui = PieceCounterGui()
